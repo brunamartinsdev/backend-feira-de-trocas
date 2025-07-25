4f55d0f43147cda802afbc8261b635c9fc9b6a8d
Bruna Martins Combat <martinsbruna815@gmail.com>
2025-07-25 20:25:59 -0300

    atualização build

--- src/server.js
+++ src/server.js
@@ -9,29 +9,32 @@ import uploadRoutes from "./routes/uploadRoutes.js";
 import categoriaRoutes from "./routes/categoriaRoutes.js";
 import notificacaoRoutes from "./routes/notificacaoRoutes.js"
 import cors from "cors";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const app = express();  
+// Ajuste para funcionar com ES Modules:
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/login', loginRoutes); 
-
-//tudo que vem depois de protectedRoutes exigirá um JWT válido
+app.use('/login', loginRoutes);
 app.use('/', protectedRoutes);
-
-//rotas de usuários comuns (protegidas pelo JWT, mas sem exigências de admin)
 app.use('/usuarios', usuarioRoutes);
 app.use('/itens', itemRoutes);
 app.use('/propostas', propostaRoutes);
 app.use('/uploads', uploadRoutes);
 app.use('/categorias', categoriaRoutes);
-
-app.use('/propostas', propostaRoutes);
-app.use('/notificacoes', notificacaoRoutes);    
-
-//rota de administração (protegida pelo JWT e exigem privilégios de admin)
+app.use('/notificacoes', notificacaoRoutes);
 app.use('/admin', adminRoutes);
 
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 const PORT = 8084;
 app.listen(PORT, () => {
